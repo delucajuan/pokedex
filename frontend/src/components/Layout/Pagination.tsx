d9d1ecae2aad997645d2ebe42dfb8ca988d8b04c
Juan De Luca <d.luca.juan@gmail.com>
2024-10-08 15:08:10 -0300

    Frontend: Change pagination size to small on xs screens

--- frontend/src/components/Layout/Pagination.tsx
+++ frontend/src/components/Layout/Pagination.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { CustomPaginationProps } from '@/types/types';
+import { useMediaQuery } from '@mui/material';
 import Pagination from '@mui/material/Pagination';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 function CustomPagination({ totalPages, currentPage }: CustomPaginationProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const isXxs = useMediaQuery('(max-width: 395px)');
+
   const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
     const params = new URLSearchParams(searchParams.toString());
     params.set('page', page.toString());
@@ -15,6 +18,7 @@ function CustomPagination({ totalPages, currentPage }: CustomPaginationProps) {
     <Pagination
       count={totalPages}
       color="primary"
+      size={isXxs ? 'small' : 'medium'}
       page={currentPage}
       onChange={handlePageChange}
     />
